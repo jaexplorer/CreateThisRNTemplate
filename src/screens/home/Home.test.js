6df2dc699077ffb15789418298600ff4496e3f1e
Andrew Sabato <andrew.sabato@sortedservices.com>
2021-12-13 15:14:00 +1100

    add test

---
+++ src/screens/home/Home.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import Home from './Home';
+
+test('renders home', () => {
+  const { container } = render(<Home />);
+  expect(container).toMatchSnapshot();
+});
